9e06539a74bf689c96add1081d966e5e2f3abe3a
novy <slnowak154@gmail.com>
2016-12-17 21:06:00 +0100

    Added logging and aws elb

--- deploy_on_aws.tf
+++ deploy_on_aws.tf
@@ -2,18 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name = "deployer-key"
-  public_key = "${file("insecure-deployer.pub")}"
-}
-
 resource "aws_security_group" "open_ports" {
   ingress {
     from_port = 0
     to_port = 65535
     protocol = "tcp"
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0"
+    ]
   }
 
   egress {
@@ -21,16 +17,47 @@ resource "aws_security_group" "open_ports" {
     to_port = 65535
     protocol = "tcp"
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0"
+    ]
   }
 }
 
 resource "aws_instance" "web" {
-  ami = "ami-50efe747"
+  ami = "ami-3ee4ec29"
   instance_type = "t2.micro"
   associate_public_ip_address = true
-  key_name = "${aws_key_pair.deployer.key_name}"
 
   vpc_security_group_ids = [
-    "${aws_security_group.open_ports.id}"]
+    "${aws_security_group.open_ports.id}"
+  ]
 }
+
+resource "aws_elb" "balancer" {
+  availability_zones = [
+    "us-east-1b"
+  ]
+
+  listener {
+    instance_port = 8080
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:8080/health"
+    interval = 30
+  }
+
+  instances = [
+    "${aws_instance.web.id}"
+  ]
+  cross_zone_load_balancing = true
+  idle_timeout = 60
+  connection_draining = true
+  connection_draining_timeout = 400
+}
+
