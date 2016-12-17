1abe8d496055ee2c037bb4d022403b47d738bc6a
novy <slnowak154@gmail.com>
2016-12-17 18:29:09 +0100

    packer script

--- deploy_on_aws.tf
+++ deploy_on_aws.tf
@@ -28,34 +28,11 @@ resource "aws_security_group" "open_ports" {
 }
 
 resource "aws_instance" "web" {
-  ami = "ami-06c4cb11"
+  ami = "ami-50efe747"
   instance_type = "t2.micro"
   associate_public_ip_address = true
   key_name = "${aws_key_pair.deployer.key_name}"
 
   vpc_security_group_ids = [
     "${aws_security_group.open_ports.id}"]
-
-  provisioner "file" {
-    source = "target/iosr-aws-autoscale-1.0-SNAPSHOT.jar"
-    destination = "/tmp/app.jar",
-    connection {
-      type = "ssh",
-      user = "ubuntu",
-      private_key = "${file("insecure-deployer")}"
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get install -y openjdk-8-jdk",
-      "nohup java -jar /tmp/app.jar &",
-      "sleep 2"
-    ],
-    connection {
-      type = "ssh",
-      user = "ubuntu",
-      private_key = "${file("insecure-deployer")}"
-    }
-  }
 }
